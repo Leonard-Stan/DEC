01537d43b1cdfeba37329826e33a5ea312e82618
LetuCode <strikerletu@gmail.com>
2024-11-13 20:48:11 +0100

    añadido todas las carpetas

---
+++ Segundo Trimestre/Ajax/Ajax_05/script.js
@@ -0,0 +1,1 @@
+alert("Esto está importado con Ajax")
---
+++ Segundo Trimestre/Ajax/Ajax_06/fecha.php
@@ -0,0 +1,8 @@
+<?php
+
+date_default_timezone_set('Europe/Madrid');
+
+$fecha = date("Y-m-d H:i:s");
+
+echo "Fecha y hora del servidor: " . $fecha;
+?>
