ebcd28d3589bcbb8008847675d854e43c25945d1
LetuCode <strikerletu@gmail.com>
2024-12-18 19:49:41 +0100

    Añadidos los dos primeros ejercicios de Effectos

---
+++ Segundo Trimestre/JQuery_Effects/JQuery_Effect_01/script.js
@@ -0,0 +1,35 @@
+$(function () {
+    
+    
+    $('#hide').on("click", function() {
+        $('#imagen').hide();
+    })
+    
+    $('#show').on("click", function() {
+        $('#imagen').show(500);
+    })
+    
+    $('#hide').on("click", function() {
+        $('#imagen').hide();
+    })
+
+    $('#fadeOut').on("click", function() {
+        $('#imagen').fadeOut(100);
+    })
+
+    $('#slideUp').on("click", function() {
+        $('#imagen').slideUp();
+    })
+
+    $('#slideDown').on("click", function() {
+        $('#imagen').slideDown();
+    })
+
+
+    $('#animate').on("click", function() {
+        $('#imagen').animate({
+           
+            top: '+=40',
+            opacity: 1},
+             1000);
+})})
