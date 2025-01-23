bb6c989f39086d774a142ad094a66ec342f50708
Leonard <adrianinformatica1920@gmail.com>
2025-01-23 12:42:33 +0100

    EXAMEN A Y B

--- Segundo Trimestre/JQuery_Effects/JQuery_Effect_05/script.js
+++ Segundo Trimestre/JQuery_Effects/JQuery_Effect_05/script.js
@@ -2,16 +2,19 @@
 $(function () {
     $("#cubo").click(function () {
         $(this)
+
             .queue(function (next) {
                 $(this).animate({ width: "200px" }, 1000); // Aumenta el ancho
                 next();
-            })
-            .delay(500) // Espera medio segundo
+            }).delay(500) // Espera medio segundo
+
+
             .queue(function (next) {
                 $(this).css("background-color", "green"); // Cambia a verde
                 next();
-            })
-            .delay(500) // Espera medio segundo
+            }).delay(500) // Espera medio segundo
+
+            
             .queue(function (next) {
                 $(this).animate({ height: "200px" }, 1000); // Aumenta la altura
                 next();
